c7e868efa845fa9a624a997da4e9bcdf84382f65
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2022-08-14 01:52:33 -0400

    added form

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ let myLibrary = [];
 
 function Book(){
   //the constructor..
+  //user's input from JS will come here, and a new book Object will be created
+
 }
 
 function addBookToLibrary(){
   //do stuff here
+}
+
+/*pop-up form from https://www.w3schools.com/howto/howto_js_popup_form.asp */
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
 }
